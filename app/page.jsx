83e62c94d13393b633f89d2a02215d270faa24c5
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-05-11 16:01:07 +0800

    update home page

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Discover & Share
+                <br className="lg:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            </h1>
+            <p className="desc text-center">Promptopia is a tool for people to discover, create and share creative prompts</p>
+        </section>
+    )
+}
+
+export default Home
