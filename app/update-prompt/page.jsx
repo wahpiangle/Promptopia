49710b3734e6ae6d28c2248a91a141ad7fadfd9c
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-05-19 11:22:29 +0800

    update to deploy

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-
+import { useSearchParams, useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { useRouter } from "next/navigation";
+import PromptCard from "./PromptCard";
 
 const ProfilePromptCard = ({ post, handleEdit, handleDelete }) => {
   return (
@@ -15,39 +16,27 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   const router = useRouter();
   return (
     <section className="w-full">
-      {name ? (
-        <>
-          <h1 className="head_text text-left">
-            <span className="blue_gradient">{name} Profile</span>
-          </h1>
-          <p className="desc text-left">{desc}</p>
-          <div className="mt-10 prompt_layout w-full">
-            <Suspense fallback={<div className="loader-container"><span className="loader"></span></div>}>
-              {data.map((post) => (
-                <ProfilePromptCard
-                  key={post._id}
-                  post={post}
-                  handleEdit={() => handleEdit && handleEdit(post)}
-                  handleDelete={() => handleDelete && handleDelete(post)}
-                />
-              ))}
-            </Suspense>
-          </div>
-        </>
-      ) : (<>
+      <>
         <h1 className="head_text text-left">
-            <span className="blue_gradient">Error 404</span>
-          </h1>
-          <p className="desc text-left">Page Not Found</p>
-          <button className="black_btn mt-8" onClick={router.push('/')}>Click Here to return to the main page</button>
-      </>)
-
-      }
+          <span className="blue_gradient">{name} Profile</span>
+        </h1>
+        <p className="desc text-left">{desc}</p>
+        <div className="mt-10 prompt_layout w-full">
+          <Suspense fallback={<div className="loader-container"><span className="loader"></span></div>}>
+            {data.map((post) => (
+              <ProfilePromptCard
+                key={post._id}
+                post={post}
+                handleEdit={() => handleEdit && handleEdit(post)}
+                handleDelete={() => handleDelete && handleDelete(post)}
+              />
+            ))}
+          </Suspense>
+        </div>
+      </>
     </section>
   );
 };
 
-
-const PromptCard = React.lazy(() => import("@components/PromptCard"));
-
 export default Profile;
+
